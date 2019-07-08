14d4fb231e428ed7cc57f759d9eb4f35820aa269
lwd <793118678>
2019-07-08 22:24:29 +0800

    COOKIES和SEDDION

--- demo3/vote/urls.py
+++ demo3/vote/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 app_name = "vote"
 
 urlpatterns = [
-    url(r'^index/$', views.index,name='index'),
-
+    url(r'^login/$',views.login,name="login"),
+    url(r'^logout/$',views.logout,name="logout"),
+    url(r'^$', views.index,name="index"),
+    url(r'^list/(\d+)/$',views.list,name="list"),
+    url(r'^result/(\d+)/$',views.result,name="result")
 ]
